0a1199cc489a7a6adcdebb9af5eaf1bf73579455
Eric Zarowny <ezarowny@gmail.com>
2023-03-13 15:18:01 -0400

    Fix options

--- scrapy_selenium/middlewares.py
+++ scrapy_selenium/middlewares.py
@@ -48,14 +48,14 @@ class SeleniumMiddleware:
 
         driver_kwargs = {
             'executable_path': driver_executable_path,
-            f'{driver_name}_options': driver_options
+            'options': driver_options
         }
 
         # locally installed driver
         if driver_executable_path is not None:
             driver_kwargs = {
                 'executable_path': driver_executable_path,
-                f'{driver_name}_options': driver_options
+                'options': driver_options
             }
             self.driver = driver_klass(**driver_kwargs)
         # remote driver
@@ -73,7 +73,7 @@ class SeleniumMiddleware:
         driver_executable_path = crawler.settings.get('SELENIUM_DRIVER_EXECUTABLE_PATH')
         browser_executable_path = crawler.settings.get('SELENIUM_BROWSER_EXECUTABLE_PATH')
         command_executor = crawler.settings.get('SELENIUM_COMMAND_EXECUTOR')
-        driver_arguments = crawler.settings.get('SELENIUM_DRIVER_ARGUMENTS')
+        driver_arguments = crawler.settings.getlist('SELENIUM_DRIVER_ARGUMENTS')
 
         if driver_name is None:
             raise NotConfigured('SELENIUM_DRIVER_NAME must be set')
@@ -137,4 +137,3 @@ class SeleniumMiddleware:
         """Shutdown the driver when spider is closed"""
 
         self.driver.quit()
-
